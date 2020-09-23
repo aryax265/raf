b3849cce73e81d266e878aa5f1100067253fe6b6
aryax265 <71759136+aryax265@users.noreply.github.com>
2020-09-24 00:08:36 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#PBS -N exampleScript 
+#PBS -l select=1:ncpus=1:mem=1gb:ngpus=1
+#PBS -l walltime=00:01:00
+#PBS -q defaultQ
+#PBS -m abe 
+#PBS -M arya.samanta@students.iiserpune.ac.in
+
+#Change into the directory where you submitted qsub
+cd $PBS_O_WORKDIR
+
+#Load modules we need
+module load cuda/8.0.44
+
+#Actually run the program
+./hello_gpu
